1d87272242f831d278e4b8a15058db8b2a62060e
kamal <kkamalakar512@gmail.com>
2024-07-28 20:41:42 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const transactions = [
+  {
+    date: "2020-02-20",
+    type: "Debit",
+    amount: 20000,
+    description: "Piano Instrument",
+  },
+
+  {
+    date: "2020-02-18",
+    type: "Debit",
+    amount: 285,
+    description: "Mug Printing with Photo",
+  },
+  {
+    date: "2020-02-18",
+    type: "Debit",
+    amount: 500,
+    description: "Snacks Party",
+  },
+  {
+    date: "2020-02-17",
+    type: "Debit",
+    amount: "600",
+    description: "Instamart",
+  },
+  {
+    date: "2020-02-17",
+    type: "Credit",
+    amount: 50000,
+    description: "Credited to Office Account",
+  },
+];
+
+export default transactions;
